6ca3b40c424034fd3c9346a37b863085b68a8c97
Sonny Recio <sonny.recio@merkle.com>
2024-11-16 12:24:51 +0800

    Filter logic by url

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,52 @@
 <script lang="ts">
 
+    const URL_TO_WATCH = "https://www.tiktok.com/en";
+
+    // improve logic. use other declarations to improve the code
+    const isUrlValid = () => {
+        const currentUrl = window.location.href;
+
+        return currentUrl === URL_TO_WATCH;
+    };
+
     $effect(() => {
         console.log("App mounted");
 
+        const observer: MutationObserver = new MutationObserver(
+            (mutationsList, observer) => {
 
-        // Step 1: Create a MutationObserver instance
-        const observer = new MutationObserver((mutationsList, observer) => {
-            // Step 2: Define a callback function to handle the mutations
-            for (const mutation of mutationsList) {
-                if (mutation.type === "childList") {
-                    // Step 4: Filter the mutations to focus on video elements
-                    mutation.addedNodes.forEach((node) => {
-                        if (node.nodeName === "VIDEO") {
-                            setTimeout(() => { //TODO: improve logic. only pause / mute the video in "for you" page only
-                                const videoEl = node as HTMLVideoElement;
-
-                                videoEl.muted = true;
-                                videoEl.pause();
-                            }, 300);
-                        } else if (node.nodeName === "DIV") { //TODO: code is not efficient here yet atm
-                            const divEl = node as HTMLElement;
-                            console.log("Div element found", divEl.classList);
-                            // if (
-                            //     [...divEl.classList].some(
-                            //         (x) =>
-                            //             x ===
-                            //                 "css-1cps6d6-BaseGridLayout-DivVerticalGridLayout" ||
-                            //             x === "e1716dta2",
-                            //     )
-                            // ) {
-                            //     console.log("Reel view found", divEl);
-                            //     divEl.style.display = "none";
-                            // }
-
-                            // not efficient, yes. we'll find other ways to improve the removal of display other than triggering it in the mutation observer multiple times
-                            const reelViewEl = document.querySelector(
-                                "div.css-1cps6d6-BaseGridLayout-DivVerticalGridLayout.e1716dta2",
-                            ) as HTMLElement;
-
-                            if (reelViewEl) {
-                                reelViewEl.style.display = "none";
-                            }
+                // Step 2: Define a callback function to handle the mutations
+                for (const mutation of mutationsList) {
+                    if (mutation.type === "childList") {
+                        if (!isUrlValid()) {
+                            return;
                         }
-                    });
+                        
+                        // Step 4: Filter the mutations to focus on video elements
+                        mutation.addedNodes.forEach((node) => {
+                            if (node.nodeName === "VIDEO") {
+                                setTimeout(() => {
+                                    //TODO: improve logic. only pause / mute the video in "for you" page only
+                                    const videoEl = node as HTMLVideoElement;
+
+                                    videoEl.muted = true;
+                                    videoEl.pause();
+                                }, 300);
+                            } else if (node.nodeName === "DIV") {
+                                // not efficient, yes. we'll find other ways to improve the removal of display other than triggering it in the mutation observer multiple times
+                                const reelViewEl = document.querySelector(
+                                    "div.css-1cps6d6-BaseGridLayout-DivVerticalGridLayout.e1716dta2",
+                                ) as HTMLElement;
+
+                                if (reelViewEl) {
+                                    reelViewEl.style.display = "none";
+                                }
+                            }
+                        });
+                    }
                 }
-            }
-        });
+            },
+        );
 
         // Step 3: Specify the target node and the configuration for the observer
         const config = { childList: true, subtree: true };
