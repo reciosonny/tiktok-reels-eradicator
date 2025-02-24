071ddebd377b7c8728499bfc0d080663061f05a0
Sonny Recio <reciosonny@gmail.com>
2025-02-24 20:38:52 +0800

    improve option stylings

--- src/browser-widgets/options/App.svelte
+++ src/browser-widgets/options/App.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import Switch from "$lib/components/Switch/Switch.svelte";
     import "./options.scss";
+
+    let showUIDisplay = $state(true);
 </script>
 
-<main>
-    <h1>Tiktok reels eradicator</h1>
-    <h2>Options</h2>
-    <div>
-        <span>**list of options here**</span>
-    </div>
-    <Switch id="thisIsId">
+<main class="px-8 pt-12">
+    <h1 class="text-center text-4xl">Tiktok reels eradicator</h1>
+    <h2 class="text-3xl">Settings</h2>
+    <Switch id="thisIsId" className="mt-4" onCheckedChange={(event) => console.log('event changed: ', event)}>
+        <span>This is now the new way</span>
+    </Switch>
+    <Switch id="another" className="mt-4">
         <span>This is now the new way</span>
     </Switch>
 </main>
--- src/lib/components/Switch/Switch.svelte
+++ src/lib/components/Switch/Switch.svelte
@@ -3,23 +3,24 @@
 
     interface Props {
         id?: string;
+        className?: string;
         children?: any;
         onCheckedChange?: (event: boolean) => void;
     }
 
-    const { id, label, children, onCheckedChange = () => {} }: Props = $props();
+    const { id, className, children, onCheckedChange = () => {} }: Props = $props();
 </script>
 
-<div class="flex items-center space-x-3">
+<div class={`flex items-center space-x-3 ${className}`}>
     <Switch.Root
         id={id}
         name="hello"
         style="background: yellow;"
-        class="border focus-visible:ring-foreground focus-visible:ring-offset-background data-[state=checked]:bg-foreground data-[state=unchecked]:bg-dark-10 data-[state=unchecked]:shadow-mini-inset dark:data-[state=checked]:bg-foreground peer inline-flex h-[36px] min-h-[36px] w-[60px] shrink-0 cursor-pointer items-center rounded-full px-[3px] transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        class={`border focus-visible:ring-foreground focus-visible:ring-offset-background data-[state=checked]:bg-foreground data-[state=unchecked]:bg-dark-10 data-[state=unchecked]:shadow-mini-inset dark:data-[state=checked]:bg-foreground peer inline-flex h-[36px] min-h-[36px] w-[60px] shrink-0 cursor-pointer items-center rounded-full px-[3px] transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`}
         onCheckedChange={onCheckedChange}
     >
         <Switch.Thumb
-            class="bg-background data-[state=unchecked]:shadow-mini dark:border-background/30 dark:bg-foreground dark:shadow-popover pointer-events-none block size-[30px] shrink-0 rounded-full transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:border dark:data-[state=unchecked]:border"
+            class="bg-background data-[state=unchecked]:shadow-mini dark:border-background/30 dark:bg-foreground dark:shadow-popover pointer-events-none block size-[30px] shrink-0 rounded-full transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:border dark:data-[state=unchecked]:border" style="background: green;"
         />
     </Switch.Root>
     <Label.Root for={id} class="text-sm font-medium">{@render children?.()}</Label.Root>
