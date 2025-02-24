9211968832df61b6d26ddfadbf524e4f17b9e0c4
Sonny Recio <reciosonny@gmail.com>
2025-02-24 23:01:33 +0800

    Improved switch component, tested radiobutton component

--- src/browser-widgets/options/App.svelte
+++ src/browser-widgets/options/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Switch from "$lib/components/Switch/Switch.svelte";
+    import { Label, RadioGroup } from "bits-ui";
     import "./options.scss";
 
     let showQuotes = $state(false);
@@ -11,7 +12,11 @@
     <section class="pt-20">
         <h2 class="text-3xl">Settings</h2>
         <section>
-            <Switch id="thisIsId" className="mt-4" onCheckedChange={(val) => showDisableFor = val}>
+            <Switch
+                id="thisIsId"
+                className="mt-4"
+                onCheckedChange={(val) => (showDisableFor = val)}
+            >
                 <span>Disable reel block</span>
             </Switch>
 
@@ -23,21 +28,74 @@
                         <p>1 hour</p>
                         <p>2 hours</p>
                     </div>
-                </section>            
+                </section>
             {/if}
         </section>
         <section>
-            <Switch id="another" className="mt-4" onCheckedChange={(event) => showQuotes = event}>
+            <Switch
+                id="another"
+                className="mt-4"
+                onCheckedChange={(event) => (showQuotes = event)}
+            >
                 <span>Show quotes (soon)</span>
             </Switch>
 
             {#if showQuotes}
                 <div class="border-solid border-2 border-black p-4 mt-4">
                     <h3 class="text-2xl">Quotes here</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, adipisci! Nobis soluta repudiandae, distinctio quam, labore dolorem reprehenderit modi harum error blanditiis officiis sequi facere iure suscipit mollitia, dignissimos quisquam?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, adipisci! Nobis soluta repudiandae, distinctio quam, labore dolorem reprehenderit modi harum error blanditiis officiis sequi facere iure suscipit mollitia, dignissimos quisquam?</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quia, adipisci! Nobis soluta repudiandae, distinctio
+                        quam, labore dolorem reprehenderit modi harum error
+                        blanditiis officiis sequi facere iure suscipit mollitia,
+                        dignissimos quisquam?
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quia, adipisci! Nobis soluta repudiandae, distinctio
+                        quam, labore dolorem reprehenderit modi harum error
+                        blanditiis officiis sequi facere iure suscipit mollitia,
+                        dignissimos quisquam?
+                    </p>
                 </div>
             {/if}
+        </section>
+        <section class="pt-10">
+            <h3>Choices</h3>
+
+            <RadioGroup.Root class="flex flex-col gap-4 text-sm font-medium">
+                <div
+                    class="text-foreground group flex select-none items-center transition-all"
+                >
+                    <RadioGroup.Item
+                        id="amazing"
+                        value="amazing"
+                        class="border-border-input bg-background hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground size-5 shrink-0 cursor-default rounded-full border transition-all duration-100 ease-in-out"
+                    />
+                    <Label.Root for="amazing" class="pl-3">Amazing</Label.Root>
+                </div>
+                <div
+                    class="text-foreground group flex select-none items-center transition-all"
+                >
+                    <RadioGroup.Item
+                        id="average"
+                        value="average"
+                        class="border-border-input bg-background hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground size-5 shrink-0 cursor-default rounded-full border transition-all duration-100 ease-in-out"
+                    />
+                    <Label.Root for="average" class="pl-3">Average</Label.Root>
+                </div>
+                <div
+                    class="text-foreground group flex select-none items-center transition-all"
+                >
+                    <RadioGroup.Item
+                        id="terrible"
+                        value="terrible"
+                        class="border-border-input bg-background hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground size-5 shrink-0 cursor-default rounded-full border transition-all duration-100 ease-in-out"
+                    />
+                    <Label.Root for="terrible" class="pl-3">Terrible</Label.Root
+                    >
+                </div>
+            </RadioGroup.Root>
         </section>
     </section>
 </main>
--- src/lib/components/Switch/Switch.svelte
+++ src/lib/components/Switch/Switch.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
     import { Label, Switch } from "bits-ui";
+    import type { HTMLAttributes } from "svelte/elements";
 
-    interface Props {
+    interface Props extends HTMLAttributes<HTMLElement> {
         id?: string;
         className?: string;
         children?: any;
         onCheckedChange?: (event: boolean) => void;
     }
 
-    const { id, className, children, onCheckedChange = () => {} }: Props = $props();
+    const { id, className, children, onCheckedChange = () => {}, ...rest }: Props = $props();
 </script>
 
 <div class={`flex items-center space-x-3 ${className}`}>
@@ -17,6 +18,7 @@
         style="background: yellow;"
         class={`border focus-visible:ring-foreground focus-visible:ring-offset-background data-[state=checked]:bg-foreground data-[state=unchecked]:bg-dark-10 data-[state=unchecked]:shadow-mini-inset dark:data-[state=checked]:bg-foreground peer inline-flex h-[36px] min-h-[36px] w-[60px] shrink-0 cursor-pointer items-center rounded-full px-[3px] transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`}
         onCheckedChange={onCheckedChange}
+        {...rest}
     >
         <Switch.Thumb
             class="bg-background data-[state=unchecked]:shadow-mini dark:border-background/30 dark:bg-foreground dark:shadow-popover pointer-events-none block size-[30px] shrink-0 rounded-full transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:border dark:data-[state=unchecked]:border" style="background: green;"
